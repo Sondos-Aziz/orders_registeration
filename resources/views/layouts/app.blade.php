bf512013615a981095028a8c7172dab0304259e0
Shimaa <shimaa1751998@gmail.com>
2020-10-24 12:12:08 +0300

    add template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,11 +19,10 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+<body style="direction: rtl">
+    <div id="app" >
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm p-0">
+                {{-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -68,13 +67,25 @@
                             </li>
                         @endguest
                     </ul>
-                </div>
+                </div> --}}
+
+
+                <div class="wrapper " >
+                    @include('layouts.partial.slidebar')
+                        {{-- <div class="main-panel" style="overflow: auto  !important"> --}}
+                            <!-- Navbar -->
+                        @include('layouts.partial.topbar')
+                            <!-- End Navbar -->
+                         @yield('content')
+                        {{-- </div> --}}
             </div>
         </nav>
 
-        <main class="py-4">
+        {{-- <main class="py-4">
             @yield('content')
-        </main>
+        </main> --}}
     </div>
+                                @include('layouts.partial.footer')
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
+});
+
+Route::get('/h', function () {
+    return view('home');
 });
 
 Auth::routes();
